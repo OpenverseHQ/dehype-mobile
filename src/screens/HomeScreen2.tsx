40e30cbae203a1d7afb4c31489b8680fa0dcce82
trandinhthang203 <tranthangkhuong203@gmail.com>
2024-12-27 21:00:28 +0700

    fix fav/home

--- src/screens/HomeScreen2.tsx
+++ src/screens/HomeScreen2.tsx
@@ -1,4 +1,5 @@
-import { Text, StyleSheet, View, SafeAreaView, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import { Text, StyleSheet, View, SafeAreaView, ScrollView, TouchableOpacity, FlatList, } from 'react-native';
+import { RefreshControl } from 'react-native';
 import React, { useState, useEffect, useRef } from 'react';
 import Header from '../components/Header';
 import CategoryCollection from '../components/CategoryCollection';
@@ -24,6 +25,7 @@ const HomeScreen2 = ({ navigation, route }: any) => {
   const [selectedTab, setSelectedTab] = useState('All');
   const { selectedAccount } = useAuthorization();
   const { handleGetAccess } = useApi();
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
 
   const fetchCategories = async () => {
@@ -35,80 +37,83 @@ const HomeScreen2 = ({ navigation, route }: any) => {
     }
   };
 
+  const fetchMarketData = async () => {
+    try {
+      if (!selectedAccount) {
+        console.log('Chưa đăng nhập...');
+        return;
+      }
+
+      const marketResponse = await api.get('/markets');
+      const allMarkets = marketResponse.data;
+
+      const auth = await handleGetAccess(selectedAccount.publicKey);
+
+      const favoriteResponse = await api.get('/search/details?fav=true');
+      const favoriteMarkets = favoriteResponse.data;
+
+      const favoritePublicKeys = favoriteMarkets.map((item: any) => item.publicKey);
+      setFavourites(favoritePublicKeys);
+
+      const allMarketsWithStats = await Promise.all(
+        allMarkets.map(async (market: any) => {
+          const statsResponse = await api.get(`/markets/${market.publicKey}/stats`);
+          return { ...market, marketStats: statsResponse.data };
+        })
+      );
+
+      const favoriteMarketsWithStats = allMarketsWithStats.filter((market: any) =>
+        favoritePublicKeys.includes(market.publicKey)
+      );
+
+      setMarketData(allMarketsWithStats);
+      setMarketFavoriteData(favoriteMarketsWithStats);
+
+    } catch (error) {
+      console.error('Error fetching market data:', error);
+    }
+  };
+
   useEffect(() => {
     fetchCategories();
-  }, [navigation]);
-
-  useEffect(() => {
-    const fetchMarketData = async () => {
-      try {
-        // Kiểm tra nếu chưa đăng nhập
-        if (!selectedAccount) {
-          console.log('Chưa đăng nhập...');
-          return;
-        }
-
-        console.log('Token hiện tại:', selectedAccount);
-
-        // Fetch all markets
-        const marketResponse = await api.get('/markets');
-        const allMarkets = marketResponse.data;
-
-        // Fetch favorite markets
-        const auth = await handleGetAccess(selectedAccount.publicKey);
-
-        const favoriteResponse = await api.get('/search/details?fav=true');
-        const favoriteMarkets = favoriteResponse.data;
-
-        // Extract favorite public keys
-        const favoritePublicKeys = favoriteMarkets.map((item: any) => item.publicKey);
-        setFavourites(favoritePublicKeys);
-
-        // Combine and fetch stats for all markets
-        const allMarketsWithStats = await Promise.all(
-          allMarkets.map(async (market: any) => {
-            const statsResponse = await api.get(`/markets/${market.publicKey}/stats`);
-            return { ...market, marketStats: statsResponse.data };
-          })
-        );
-
-        const favoriteMarketsWithStats = allMarketsWithStats.filter((market: any) =>
-          favoritePublicKeys.includes(market.publicKey)
-        );
-
-        setMarketData(allMarketsWithStats);
-        setMarketFavoriteData(favoriteMarketsWithStats);
-
-      } catch (error) {
-        console.error('Error fetching market data:', error);
-      }
-    };
-
     fetchMarketData();
   }, [navigation]);
 
-
+  const onRefresh = async () => {
+    setIsRefreshing(true);
+    await fetchMarketData();
+    setIsRefreshing(false);
+  };
 
   const filterByCategory = (category: string) => {
     return marketData.filter((market: any) => market.category === category);
   };
 
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Header />
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefreshing}
+            onRefresh={onRefresh}
+          />
+        }
+      >
         <View style={styles.container}>
           <View><Text style={styles.text_cate}>Category</Text></View>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            {categories.map((category) => (
-              <Category key={category.id} id={category.id} nameCategory={category.name} coverUrl={category.coverUrl} />
-            ))}
-          </ScrollView>
+          <FlatList
+            data={categories}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <Category key={item.id} id={item.id} nameCategory={item.name} coverUrl={item.coverUrl} />
+            )}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
         </View>
 
         <View style={styles.tabContainer}>
-          {/* Thay đổi thứ tự hiển thị các tab */}
           <TouchableOpacity
             style={[styles.tab, selectedTab === 'All' && styles.activeTab]}
             onPress={() => setSelectedTab('All')}
@@ -140,85 +145,67 @@ const HomeScreen2 = ({ navigation, route }: any) => {
           </TouchableOpacity>
         </View>
 
-        <View style={styles.trendingSection}>
-          {selectedTab === 'All' ? (
-            <ScrollView horizontal={false} showsVerticalScrollIndicator={false}>
-              {marketData.map((market: any) => (
-                <CardItem
-                  key={market.publicKey}
-                  publicKey={market.publicKey}
-                  title={market.title}
-                  coverUrl={market.coverUrl}
-                  participants={market.participants}
-                  totalVolume={market.totalVolume}
-                  marketStats={market.marketStats}
-                  favourites={favourites}
-                />
-              ))}
-            </ScrollView>
-          ) : selectedTab === 'Trending' ? (
-            <>
-              <CategoryCollection nameCategory='Sports' />
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                {filterByCategory('Sports').map((market: any, index: number) => (
-                  <CardItemTrend key={market.id} nameMarket={market.name} outcome={market.outcome} id={market.id} LikeCount={market.likes} />
-                ))}
-              </ScrollView>
-              <CategoryCollection nameCategory='Technology' />
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                {filterByCategory('Technology').map((market: any, index: number) => (
-                  <CardItemTrend key={market.id} nameMarket={market.name} outcome={market.outcome} id={market.id} LikeCount={market.likes} />
-                ))}
-              </ScrollView>
-              <CategoryCollection nameCategory='Finance' />
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                {filterByCategory('Finance').map((market: any, index: number) => (
-                  <CardItemTrend key={market.id} nameMarket={market.name} outcome={market.outcome} id={market.id} LikeCount={market.likes} />
-                ))}
-              </ScrollView>
-              <CategoryCollection nameCategory='Politics' />
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                {filterByCategory('Politics').map((market: any, index: number) => (
-                  <CardItemTrend key={market.id} nameMarket={market.name} outcome={market.outcome} id={market.id} LikeCount={market.likes} />
-                ))}
-              </ScrollView>
-            </>
-          ) : selectedTab === 'Newest' ? (
-            <>
-              <CategoryCollection nameCategory='Entertaiment' />
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-              </ScrollView>
-              <CategoryCollection nameCategory='Finance' />
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-              </ScrollView>
-              <CategoryCollection nameCategory='News' />
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-              </ScrollView>
-            </>
-          ) : (
-            <>
-              <ScrollView horizontal={false} showsVerticalScrollIndicator={false}>
-                {marketFavoriteData.map((market: any) => (
-                  <CardItem
-                    key={market.publicKey}
-                    publicKey={market.publicKey}
-                    title={market.title}
-                    coverUrl={market.coverUrl}
-                    participants={market.participants}
-                    totalVolume={market.totalVolume}
-                    marketStats={market.marketStats}
-                    favourites={favourites}
-                  />
-                ))}
-              </ScrollView>
-            </>
+        <FlatList
+          style={{ marginLeft: 10 }}
+          data={selectedTab === 'All' ? marketData : selectedTab === 'Favorite' ? marketFavoriteData : []}
+          keyExtractor={(item) => item.publicKey}
+          renderItem={({ item }) => (
+            <CardItem
+              key={item.publicKey}
+              publicKey={item.publicKey}
+              title={item.title}
+              coverUrl={item.coverUrl}
+              participants={item.participants}
+              totalVolume={item.totalVolume}
+              marketStats={item.marketStats}
+              favourites={favourites}
+            />
           )}
-        </View>
+          showsVerticalScrollIndicator={false}
+        />
+
+        {selectedTab === 'Trending' && (
+          <>
+            <CategoryCollection nameCategory='Sports' />
+            <FlatList
+              data={filterByCategory('Sports')}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <CardItemTrend
+                  key={item.id}
+                  nameMarket={item.name}
+                  outcome={item.outcome}
+                  id={item.id}
+                  LikeCount={item.likes}
+                />
+              )}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+
+            />
+            <CategoryCollection nameCategory='Technology' />
+            <FlatList
+              data={filterByCategory('Technology')}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <CardItemTrend
+                  key={item.id}
+                  nameMarket={item.name}
+                  outcome={item.outcome}
+                  id={item.id}
+                  LikeCount={item.likes}
+                />
+              )}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            />
+            {/* Add more categories similarly */}
+          </>
+        )}
       </ScrollView>
     </SafeAreaView>
   );
 };
-
 
 
 export default HomeScreen2;
@@ -226,8 +213,7 @@ export default HomeScreen2;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
-    paddingBottom: 0,
+    padding: 5,
     backgroundColor: '#fff',
   },
   text_cate: {
